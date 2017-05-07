cff93e860aa1147c5bc574ff9d2cac323bfc1e0b
bartek <mail@bartekpawlowski.pl>
2017-05-07 20:59:33 +0200

    Add new Activities, update MainActivity with Listeners

--- app/src/main/java/pl/bartekpawlowski/musicalstructure/MainActivity.java
+++ app/src/main/java/pl/bartekpawlowski/musicalstructure/MainActivity.java
@@ -1,7 +1,10 @@
 package pl.bartekpawlowski.musicalstructure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewGroup playButton = (ViewGroup) findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playIntent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(playIntent);
+            }
+        });
+
+        ViewGroup randomButton = (ViewGroup) findViewById(R.id.randomButton);
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent randomIntent = new Intent(MainActivity.this, RandomActivity.class);
+                startActivity(randomIntent);
+            }
+        });
+
+        ViewGroup playlistButton = (ViewGroup) findViewById(R.id.playlistButton);
+        playlistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playlistIntent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(playlistIntent);
+            }
+        });
+
+        ViewGroup settingsButton = (ViewGroup) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(settingsIntent);
+            }
+        });
     }
 }
