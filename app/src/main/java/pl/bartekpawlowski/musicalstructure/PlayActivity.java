38228670522e21c23e49475bcd5b5d72434d1a93
bartek <mail@bartekpawlowski.pl>
2017-06-13 18:25:46 +0200

    Add external library - exoplayer2

--- app/src/main/java/pl/bartekpawlowski/musicalstructure/PlayActivity.java
+++ app/src/main/java/pl/bartekpawlowski/musicalstructure/PlayActivity.java
@@ -2,6 +2,7 @@ package pl.bartekpawlowski.musicalstructure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.exoplayer2.core.*;
 
 public class PlayActivity extends AppCompatActivity {
 
